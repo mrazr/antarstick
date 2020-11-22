a7fca655dc9af4a7c8065d93aaece7f3d1e618ae
Radoslav Mráz <radoslav.mraz95@gmail.com>
2020-11-22 21:41:50 +0100

    install script for packages and run script for linux

---
+++ antarstick.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source venv/bin/activate
+
+venv/bin/python3 mainwindow.py
+
+deactivate  
---
+++ antarstick_install_linux.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# create new python virtual environment
+python3 -m venv venv
+
+./venv/bin/pip3 install -r requirements.txt
