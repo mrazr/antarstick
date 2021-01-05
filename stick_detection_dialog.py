b2e9660922588cc5c7444e44bdbac84512044ab7
Radoslav Mraz <radoslav.mraz95@gmail.com>
2021-01-05 19:58:12 +0100

    deleted unused dialog

--- stick_detection_dialog.py
+++
@@ -1,9 +0,0 @@
-from camera_processing.widgets.stick_detection_params import  Ui_StickDetectionParams
-
-from PyQt5 import QtWidgets
-
-
-class StickDetectionDialog(QtWidgets.QDialog, Ui_StickDetectionParams):
-    def __init__(self, *args, obj=None, **kwargs):
-        super(StickDetectionDialog, self).__init__(*args, **kwargs)
-        self.setupUi(self)
